903ae7e7cb8402647a3887f9132bac68e9727a31
taiseiiiii <66340131+taiseiiiii@users.noreply.github.com>
2021-09-02 02:15:06 +0900

    fix(rename dir name)

--- frontend/todo/src/Theme.tsx
+++ frontend/todo/src/Theme.tsx
@@ -1,11 +1,13 @@
 import { extendTheme } from "@chakra-ui/react";
+import { FaCashRegister } from "react-icons/fa";
 
 export const theme = extendTheme({
   styles: {
     global: {
       body: {
         bg:NaN,
-        bgImage:"url('https://source.unsplash.com/random')"
+        bgImage:"url('https://source.unsplash.com/random')",
+        bgSize:"cover"
       }
     }
   }
